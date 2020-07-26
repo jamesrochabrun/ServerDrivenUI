765dd1d6fb591ada7c2a95c882ed5c01388122e0
james rochabrun <jamesrochabrun@gmail.com>
2020-07-26 11:17:45 -0700

    Initial Commit

---
+++ ServerSideDrivenSwiftUI/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ServerSideDrivenSwiftUI
+//
+//  Created by James Rochabrun on 7/26/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ServerSideDrivenSwiftUI/ServerSideDrivenSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServerSideDrivenSwiftUIApp.swift
+//  ServerSideDrivenSwiftUI
+//
+//  Created by James Rochabrun on 7/26/20.
+//
+
+import SwiftUI
+
+@main
+struct ServerSideDrivenSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
